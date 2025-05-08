1506aaa7be5696a514127cf26b72a389044db6dc
Jeevesh0207 <r27abbit@gmail.com>
2025-05-08 23:34:05 +0530

    fix: update sameSite cookie attribute from lax to None for cross-site requests

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -233,7 +233,7 @@ async function loginUser(req,res){
     const options = {
       httpOnly: true,
       secure: false,
-      sameSite: 'lax',
+      sameSite: 'None',
       maxAge: 24 * 60 * 60 * 1000,
       path: '/'
     }
